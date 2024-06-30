46fb0e174eb7992bb5e8d89044266c134d7d6df2
FAYAMA Dorcas <fayamamassahibadorcas@gmail.com>
2024-06-30 20:20:43 +0000

    ecran decreation du compte d'un utilisatuer et enregistrement dans la bse de donnee

--- userModule.py
+++ userModule.py
@@ -1,1 +1,7 @@
 # This is the userModule, both for library import and custom classes definition
+# librairie python que l'on importe ici
+import bcrypt
+#bcrypt permet de generer du sel(des caractere)
+make_salt = lambda : bcrypt.gensalt()
+#hasher le mot de passe
+crypt_pass = lambda passw, salt : bcrypt.hashpw(passw.encode('utf-8'), salt)
